0feb3f26b6b5f6e5938dc219c897ed82b107712f
jawadnajih27-spec <J.najih4208@uca.ac.ma>
2026-03-07 23:04:49 +0000

    Update admin.js
    Signed-off-by: jawadnajih27-spec <J.najih4208@uca.ac.ma>

--- assets/js/assets/js/assets/js/admin.js
+++ assets/js/assets/js/assets/js/admin.js
@@ -1,1 +1,19 @@
-// Admin functions
+function addProduct(){
+
+const name = document.getElementById("name").value
+const price = document.getElementById("price").value
+const image = document.getElementById("image").value
+
+const product = {
+
+name,
+price,
+image
+
+}
+
+console.log(product)
+
+alert("Product ready to send to database")
+
+  }
