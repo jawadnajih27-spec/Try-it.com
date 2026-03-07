1784e37cdac7eb9c0c1b56ffae0f364fa4628405
jawadnajih27-spec <J.najih4208@uca.ac.ma>
2026-03-07 23:03:23 +0000

    Update cart.js
    Signed-off-by: jawadnajih27-spec <J.najih4208@uca.ac.ma>

--- assets/js/assets/js/cart.js
+++ assets/js/assets/js/cart.js
@@ -1,1 +1,35 @@
-// Cart system
+async function loadCart(){
+
+let cart = JSON.parse(localStorage.getItem("cart")) || []
+
+const res = await fetch("data/products.json")
+
+const products = await res.json()
+
+const container = document.getElementById("cartItems")
+
+let total = 0
+
+cart.forEach(id=>{
+
+const p = products.find(x=>x.id===id)
+
+total += p.price
+
+container.innerHTML+=`
+
+<div>
+
+${p.name} - $${p.price}
+
+</div>
+
+`
+
+})
+
+document.getElementById("total").innerText="Total: $"+total
+
+}
+
+loadCart()
